d753b4d77ce5910f025e90a6024b3a42f7a105fb
Perbi-onasis <onasisyawperbi@gmail.com>
2023-06-09 14:45:00 +0100

    setup carousel section to scroll from right to left

--- script.js
+++ script.js
@@ -25,3 +25,50 @@ let movies = [
         image: 'images/slider 5.PNG'
     }
 ]
+
+// Code to setup the carousel section
+let carousel = document.querySelector('.carousel');
+let sliders = [];
+
+let slideIndex = 0;
+
+const createSlide = () => {
+    if(slideIndex >= movies.length){
+        slideIndex = 0;
+    }
+
+    // creating DOM element
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+
+    //attaching all elements
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    slide.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+
+    //setting up images which is in slider-data.js
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+
+    //setting elements
+    slide.className = 'slider';
+    content.className = 'slider-content';
+    h1.className = 'movie-title';
+    p.className = 'movie-desc';
+
+    sliders.push(slide);
+
+    //adding sliding effect
+    if(sliders.length){
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length -2)}px)`;
+    }
+
+}
