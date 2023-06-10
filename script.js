68e8a91efc3d984ef592c1f8d3b744d1ce71249f
Perbi Onasis Obuobi-Dei <65021040+perbi-onasis@users.noreply.github.com>
2023-06-10 14:10:36 +0100

    comment scroll script

--- script.js
+++ script.js
@@ -27,59 +27,59 @@ let movies = [
 ]
 
 // Code to setup the carousel section
-let carousel = document.querySelector('.carousel');
-let sliders = [];
+// let carousel = document.querySelector('.carousel');
+// let sliders = [];
 
-let slideIndex = 0;
+// let slideIndex = 0;
 
-const createSlide = () => {
-    if(slideIndex >= movies.length){
-        slideIndex = 0;
-    }
+// const createSlide = () => {
+//     if(slideIndex >= movies.length){
+//         slideIndex = 0;
+//     }
 
     // creating DOM element
-    let slide = document.createElement('div');
-    let imgElement = document.createElement('img');
-    let content = document.createElement('div');
-    let h1 = document.createElement('h1');
-    let p = document.createElement('p');
+    // let slide = document.createElement('div');
+    // let imgElement = document.createElement('img');
+    // let content = document.createElement('div');
+    // let h1 = document.createElement('h1');
+    // let p = document.createElement('p');
 
     //attaching all elements
-    imgElement.appendChild(document.createTextNode(''));
-    h1.appendChild(document.createTextNode(movies[slideIndex].name));
-    p.appendChild(document.createTextNode(movies[slideIndex].des));
-    content.appendChild(h1);
-    slide.appendChild(p);
-    slide.appendChild(content);
-    slide.appendChild(imgElement);
-    carousel.appendChild(slide);
+    // imgElement.appendChild(document.createTextNode(''));
+    // h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    // p.appendChild(document.createTextNode(movies[slideIndex].des));
+    // content.appendChild(h1);
+    // slide.appendChild(p);
+    // slide.appendChild(content);
+    // slide.appendChild(imgElement);
+    // carousel.appendChild(slide);
 
     //setting up images which is in slider-data.js
-    imgElement.src = movies[slideIndex].image;
-    slideIndex++;
+    // imgElement.src = movies[slideIndex].image;
+    // slideIndex++;
 
     //setting elements
-    slide.className = 'slider';
-    content.className = 'slider-content';
-    h1.className = 'movie-title';
-    p.className = 'movie-desc';
+    // slide.className = 'slider';
+    // content.className = 'slider-content';
+    // h1.className = 'movie-title';
+    // p.className = 'movie-desc';
 
-    sliders.push(slide);
+    // sliders.push(slide);
 
-    //adding sliding effect
-    if(sliders.length){
-        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length -2)}px)`;
-    }
+    // adding sliding effect
+    // if(sliders.length){
+    //     sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length -2)}px)`;
+    // }
 
-}
+// }
 
 // code to make it scroll from the first element to the last and set interval 
-for(let i = 0; i < 6; i++){
-    createSlide();
-}
-setInterval(() => {
-    createSlide();
-}, 3000);
+// for(let i = 0; i < 6; i++){
+//     createSlide();
+// }
+// setInterval(() => {
+//     createSlide();
+// }, 3000);
 
 // video cards (in this card, a video will play when card hovered)
 const videoCards = [...document.querySelectorAll('.video-card')];
